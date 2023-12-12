e0cd1e535bcd3f21992de9a433343066bfb872dd
rivitch <rivitch@rambler.ru>
2023-12-12 20:46:26 +0400

    over

--- sem3/sem3app/urls.py
+++ sem3/sem3app/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
 from . import views
+# Sem3
+from .views import helloView, HelloView
+
+from .views import year_post, MonthPost, post_detail, my_view, view_client
 """
 Импортируем уже знакомую функцию path и файл с представлениями из
 текущего каталога (импорт через точку).
@@ -16,4 +20,10 @@ urlpatterns = [
         path('user/', views.user_name),# name='author_name'
         path('order/', views.order_name), # name='about'
         path('product/', views.product_name),
+        path('hello/', helloView, name='hello'),# указываем имя функции в качестве обработчика запроса
+        path('hello2/', HelloView.as_view(), name='hello2'),#используем метод as_view() класса! HelloView для создания объекта-обработчика запроса
+        path('user_year/<int:year>/', year_post, name='year_post'),
+        path('my_view/', my_view, name='my_view'),
+        path('client/<int:day>/',view_client,name='client'),
+
         ]
--- sem3/sem3app/views.py
+++ sem3/sem3app/views.py
@@ -2,20 +2,62 @@ from django.shortcuts import render
 import logging
 # Create your views here.
 from django.http import HttpResponse
+
 from random import *
 from .models import User, Product, Order
+# Sem3
+from django.views import View
+
+
 
 logger = logging.getLogger(__name__)
 
+def helloView(request):
+    logger.info('helloView def page accessed')
+    return HttpResponse('Hello World from function!') 
+
+class HelloView(View):
+    def get(self, request):
+        logger.info('HelloView class page accessed')
+        return HttpResponse("Hello World from class!")
+    
+def year_post(request, year):        # Представление через функцию возвращает HttpResponse
+    text = "sdgfhgjhkj"
+    ... # формируем статьи за год
+    return HttpResponse(f"Posts from {year}<br>{text}")
+
+# Представление через класс возвращает HttpResponse
+class MonthPost(View):
+    def get(self, request, year, month):
+        text = "123456789"
+        ... # формируем статьи за год и месяц
+        return HttpResponse(f"Posts from {month}/{year}<br>{text}")
+    
+# Представление через функцию возвращает JSON    
+from django.http import JsonResponse
+
+def post_detail(request, year, month, slug):
+    # Формируем статьи за год и месяц по идентификатору. Пока обойдёмся без запросов к базе данных
+    post = {
+        "year": year,
+        "month": month,
+        "slug": slug,
+        "title": "Кто быстрее создаёт списки в Python, list() или []",
+        "content": "В процессе написания очередной программы задумался над тем, какой способ создания списков в Python работает быстрее..."
+        }
+    return JsonResponse(post, json_dumps_params={'ensure_ascii': False}) # текст в кодировке UTF-8, а не в ASCII
+
+
 def index(request):
     logger.info('DJANGO_SEM3 page accessed')
-    return HttpResponse('ВЫ НА СТРАНИЦЕ СЕМИНАРА 3. DJANGO  Using the URLconf defined in sem3.urls, Django tried these URL patterns, in this order: admin/   или  sem3app/user/   или  sem3app/order/   или  sem3app/product/' 
+    return HttpResponse(f'ВЫ НА СТРАНИЦЕ СЕМИНАРА 3. DJANGO<br>Using the URLconf defined in sem3.urls, Django tried these URL patterns, in this order:<br> admin/<br>sem3app/user/<br>sem3app/order/<br>sem3app/product/' 
     # return HttpResponse('ВЫ НА СТРАНИЦЕ СЕМИНАРА 3.' 
     # DJANGO  Using the URLconf defined in sem3.urls, Django tried these URL patterns, in this order:'
     # admin/
     # sem3app/ user/
     # sem3app/ order/
     # sem3app/ product/
+    #return HttpResponse(f"Posts from {year}<br>{text}")
 )
 
 def user_name(request):
@@ -40,3 +82,12 @@ def order_name(request):
     order.save()
     logger.info('ОБНОВЛЕНА БАЗА ЗАКАЗОВ')   
     return HttpResponse('ОБНОВЛЕНА БАЗА ЗАКАЗОВ')  
+
+
+def my_view(request):
+    context = {"name": "John"}
+    return render(request, "sem3app/index_sem3app.html", context)
+
+def view_client(requests,day_):
+    context = {"name": "John"}
+    return render(requests, "sem3app/product.html", context)
